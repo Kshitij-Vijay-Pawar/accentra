9651bd63ec956d7852bc5d2b182530ca151a1308
Kshitij <kshitij.vijay.pawar@gmail.com>
2025-03-30 23:07:37 +0530

    Add QuestionBubble, Challenge, and Footer components; update package.json to include react-use

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,9 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
     initialPercentage: number;
@@ -24,7 +27,54 @@ export const Quiz = ({
 
     const [percentage, setPercentage] = useState(initialPercentage);
     const [hearts, setHearts] = useState(initialHearts);
+    const [challenges] = useState(initialLessonChallenges);
+    const [activeIndex, setActiveIndex] = useState(()=>{
+        const uncompletedIndex = challenges.findIndex((challenge)=>!challenge.completed);
+        return uncompletedIndex == -1 ?  0: uncompletedIndex;
+    });
 
+
+    const [selectedOption, setSelectedOption] = useState<number>();
+    const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+    
+
+    const challenge = challenges[activeIndex];
+
+
+    const title = challenge.type === "ASSIST" 
+        ? "Select the correct meaning" 
+        : challenge.question;
+    const options = challenge?.challengeOptions ?? [];
+
+
+    const onNext = () => {
+        setActiveIndex((current) => current + 1);
+    };
+    const onSelect = (id: number) => {
+        if(status !== "none") return;
+        setSelectedOption(id);
+    }
+    const onContinue = () => {
+        if(!selectedOption) return;
+        if(status === "wrong"){
+            setStatus("none")
+            setSelectedOption(undefined)
+            return;
+        }
+        if(status === "correct"){
+            onNext();
+            setStatus("none")
+            setSelectedOption(undefined)
+            return;
+        }
+        const correctOption = options.find((option) => option.correct);
+
+        if (correctOption && correctOption.id === selectedOption){
+            alert("Correct Option!");
+        } else {
+            alert("Incorrect Option!");
+        }
+    };
 
     return (
         <>
@@ -33,6 +83,34 @@ export const Quiz = ({
                 percentage={percentage}
                 hasActiveSubscription={!!userSubscription?.isActive}
             /> 
+            <div className="flex-1">
+                <div className="h-full flex items-center justify-center">
+                    <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+                        <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+                            {title}
+                        </h1>
+                        <div>
+                            {challenge.type === "ASSIST" && (
+                                <QuestionBubble question={challenge.question}/>
+                            )}
+                            <Challenge 
+                                options={options}
+                                onSelect={onSelect}
+                                status={status}
+                                selectedOption={selectedOption}
+                                disabled={false}
+                                type={challenge.type}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer 
+                disabled={!selectedOption}
+                status={status}
+                onCheck={onContinue}
+
+            />
         </>
     );
 };
